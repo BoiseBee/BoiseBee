0d3356149338bf44de60738bee5f0bb416a963b1
dragonbeast1217 <sihuhwang1217@gmail.com>
2021-02-05 08:36:57 -0700

    Fixed loop

--- Sihu/test.py
+++ Sihu/test.py
@@ -1,13 +1,22 @@
-while True:
-	try:
-		num = int(input('Type a number: ')) #Gets user input
-		num2 = int(input('Number two: ')) #Gets 2nd number
-	except:
-		print('Please type in a number')
+
+Status = True
+try:
+	while Status:
+			num = int(input('Type a number: ')) #Gets user input
+			num2 = int(input('Number two: ')) #Gets 2nd number
+
+			if type(num) == int:
+				Status = False
+			elif type(num2) == int:
+				Status = False
+
+	print(num + num2)
+	print(num / num2)
+
+except:
+	print('Something has gone wrong, please try again')
 
 
-print(num+num2)
-print(num/num2)
 
 
 
