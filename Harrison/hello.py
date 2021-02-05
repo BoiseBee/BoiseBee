ec02706de2d1ef5e14dfe4415163e14abe4dceeb
Harry-One1 <harrisonauth@gmail.com>
2021-02-04 20:53:38 -0700

    added/divided/multiplied/subtracted number outputs

--- Harrison/hello.py
+++ Harrison/hello.py
@@ -1,1 +1,8 @@
-print("Hello world")
+num1 = int(input("Type a number: ")) #Gets user input
+
+num2 = int(input("Type a number: ")) #Gets 2nd number
+
+print(num1+num2)
+print(num1/num2) 
+print(num1-num2)
+print(num1*num2)
