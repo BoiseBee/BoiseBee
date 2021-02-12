3462e13455aee839caaf9cc799ac16257a171971
NathanPearl07 <Nathan.Pearl@capboise.org>
2021-02-11 20:53:39 -0700

    loops while for

---
+++ spencer/while.py
@@ -0,0 +1,20 @@
+import time
+
+x=10
+#while x >= 0:
+#	print(x)
+#	x = x-1
+
+#	time.sleep(1)
+
+def addfour(u):
+"""
+adds four to input
+"""
+	u = u + 4
+	return u
+
+while x < 100:
+	x = x + 4
+#	x =  addfour(x)
+	print(x)
