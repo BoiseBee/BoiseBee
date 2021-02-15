190966b85843fef354f99ed5e0aa230712010bc7
Harrison <harrisonauth@gmail.com>
2021-02-14 16:56:10 -0800

    finished user input

--- Harrison/hello.py
+++ Harrison/hello.py
@@ -1,26 +1,26 @@
 while True:
-num1 = int(input("Type a number: ")) #Gets user input
+    num1 = int(input("Type a number: ")) #Gets user input
 
-num2 = int(input("Type a number: ")) #Gets 2nd number
+    num2 = int(input("Type a number: ")) #Gets 2nd number
 
-print(num1+num2)
-print(num1/num2) 
-print(num1-num2)
-print(num1*num2)
+    print(num1+num2)
+    print(num1/num2) 
+    print(num1-num2)
+    print(num1*num2)
 
-print(" ")
+    print(" ")
 
-print("Thank you for using the calculator")
+    print("Thank you for using the calculator")
 
-print(" ")
+    print(" ")
 
-    while True:
-        answer = str(input('Would you like to use the calculator again? (y/n): '))
-        if answer in ('y', 'n'):
+        while True:
+            answer = str(input('Would you like to use the calculator again? (y/n): '))
+            if answer in ('y', 'n'):
+                break
+            print("invalid input.")
+        if answer == 'y':
+            continue
+        else:
+            print("Goodbye")
             break
-        print("invalid input.")
-    if answer == 'y':
-        continue
-    else:
-        print("Goodbye")
-        break
