de05ae32bbaa783a0e75ac1bd062774311095621
Debian <debian@debian9.linuxvmimages.local>
2021-02-18 20:25:12 -0500

    user input 5

--- Harrison/hello.py
+++ Harrison/hello.py
@@ -3,13 +3,12 @@ def program():
         num1 = int(input("Type a number: ")) #Gets user input
 
         num2 = int(input("Type a number: ")) #Gets 2nd number
-<<<<<<< HEAD
-    
+
         print("addition")
         print(num1+num2)
         print(" ")
         print("division")
-        print(num1/num2) 
+        print(num1/num2)
         print(" ")
         print("subtraction")
         print(num1-num2)
@@ -23,24 +22,6 @@ def program():
 
         print(" ")
 
-=======
-
-        print (str(Addition))
-        print(num1+num2)
-        print (str(Division))
-        print(num1/num2)
-        print (str(Subtraction))
-        print(num1-num2)
-        print (str(multiplication))
-        print(num1*num2)
-
-        print(" ")
-
-        print("Thank you for using the calculator")
-
-        print(" ")
-
->>>>>>> 0bbd757c3ee7ed2644f202d15b8352371aa59366
         answer = str(input('Would you like to use the calculator again? (y/n): '))
         if answer in ('y', 'n'):
             break
@@ -50,9 +31,4 @@ def program():
         else:
             print("Goodbye")
             break
-
-<<<<<<< HEAD
 program()
-=======
-program()
->>>>>>> 0bbd757c3ee7ed2644f202d15b8352371aa59366
