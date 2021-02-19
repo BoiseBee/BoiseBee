52d6490f9527058805dcf5214528793fc51784f2
Debian <debian@debian9.linuxvmimages.local>
2021-02-18 20:17:20 -0500

    user input 4

--- Harrison/hello.py
+++ Harrison/hello.py
@@ -1,26 +1,35 @@
-while True:
-    num1 = int(input("Type a number: ")) #Gets user input
+def program():
+    while True:
+        num1 = int(input("Type a number: ")) #Gets user input
 
-    num2 = int(input("Type a number: ")) #Gets 2nd number
+        num2 = int(input("Type a number: ")) #Gets 2nd number
+    
+        print("addition")
+        print(num1+num2)
+        print(" ")
+        print("division")
+        print(num1/num2) 
+        print(" ")
+        print("subtraction")
+        print(num1-num2)
+        print(" ")
+        print("multiplication")
+        print(num1*num2)
 
-    print(num1+num2)
-    print(num1/num2) 
-    print(num1-num2)
-    print(num1*num2)
+        print(" ")
 
-    print(" ")
+        print("Thank you for using the calculator")
 
-    print("Thank you for using the calculator")
+        print(" ")
 
-    print(" ")
-
-while True:
-    answer = str(input('Would you like to use the calculator again? (y/n): '))
+        answer = str(input('Would you like to use the calculator again? (y/n): '))
         if answer in ('y', 'n'):
             break
-            print("invalid input.")
+            print("invalid input")
         if answer == 'y':
-            continue
+            program()
         else:
             print("Goodbye")
             break
+
+program()
