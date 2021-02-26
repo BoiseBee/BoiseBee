537478b8088ea3ec636cc7f4a690510dccfe0998
NathanPearl07 <Nathan.Pearl@capboise.org>
2021-02-25 19:07:42 -0700

    Fixed if statements

--- Harrison/hello.py
+++ Harrison/hello.py
@@ -1,34 +1,40 @@
 def program():
-    while True:
-        num1 = int(input("Type a number: ")) #Gets user input
+    pass
+    
+    
+while True:
+	try:
+		num1 = int(input("Type a number: ")) #Gets user input
+		num2 = int(input("Type a number: ")) #Gets 2nd number
+		
 
-        num2 = int(input("Type a number: ")) #Gets 2nd number
+		print("addition")
+		print(num1+num2)
+		print(" ")
+		print("division")
+		print(num1/num2)
+		print(" ")
+		print("subtraction")
+		print(num1-num2)
+		print(" ")
+		print("multiplication")
+		print(num1*num2)
 
-        print("addition")
-        print(num1+num2)
-        print(" ")
-        print("division")
-        print(num1/num2)
-        print(" ")
-        print("subtraction")
-        print(num1-num2)
-        print(" ")
-        print("multiplication")
-        print(num1*num2)
+		print(" ")
 
-        print(" ")
+		print("Thank you for using the calculator")
 
-        print("Thank you for using the calculator")
+		print(" ")
+		
+	except ValueError:
+		print("You need to input number...")
 
-        print(" ")
+	answer = str(input('Would you like to use the calculator again? (y/n): '))
+	if answer in ('y'):
+		if answer == "y":
+		    program()
+	else:
+		print("Goodbye")
+		break
 
-        answer = str(input('Would you like to use the calculator again? (y/n): '))
-        if answer in ('y', 'n'):
-            break
-            print("invalid input")
-        if answer == 'y':
-            program()
-        else:
-            print("Goodbye")
-            break
-program()
+#program()
